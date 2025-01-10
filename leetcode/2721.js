26bdd743682670cc96fe1c42fbbc0e38993fbff6
jamiul <jamiulewu24@gmail.com>
2025-01-10 19:24:33 +0600

    Promise all in js

---
+++ leetcode/2721.js
@@ -0,0 +1,29 @@
+/**
+ * @param {Array<Function>} functions
+ * @return {Promise<any>}
+ */
+var promiseAll = function(functions) {
+    return new Promise((resolve, reject) => {
+        let results = [];
+        let pending = functions.length;
+
+        functions.forEach((fn, i) => {
+            fn().then(value => {
+                results[i] = value;
+                pending--;
+
+                if(pending === 0) {
+                    resolve(results);
+                }
+            }).catch(error => {
+                reject(error);
+            })
+        })
+
+    })
+};
+
+/**
+ * const promise = promiseAll([() => new Promise(res => res(42))])
+ * promise.then(console.log); // [42]
+ */
